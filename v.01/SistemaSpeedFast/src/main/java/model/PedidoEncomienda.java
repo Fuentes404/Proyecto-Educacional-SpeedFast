d46a7ac8aaee788206b3cdfb4373e73dad71db42
Fuentes404 <claudioaguilar1996@icloud.com>
2026-08-13 22:53:55 -0400

    pc
    Ingreso de documentos

---
+++ v.01/SistemaSpeedFast/src/main/java/model/PedidoEncomienda.java
@@ -0,0 +1,56 @@
+package model;
+
+public class PedidoEncomienda extends Pedido {
+    // Atributos
+    private double peso;
+    private double volumen;
+
+    // Constructor
+    public PedidoEncomienda(String idPedido, String cliente, String direccion,
+                            double peso, double volumen) {
+        super(idPedido, cliente, direccion);
+        this.peso = peso;
+        this.volumen = volumen;
+        setTipoPedido("Encomienda");
+    }
+
+    // Metodo Getter and Setter
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public double getVolumen() {
+        return volumen;
+    }
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+    // Metodo Asignar Repartidor (sobreescritura: valida solo el peso)
+    @Override
+    public void asignarRepartidor() {
+        // Condicion: peso >= 100 k
+        if (peso >= 100) {
+            System.out.println("Pedido " + getIdPedido() + " - Encomienda: Peso validado: " +
+                    getPeso() + " kg. Buscando repartidor para carga.");
+        } else {
+            System.out.println("Pedido " + getIdPedido() + " - Encomienda: Buscando repartidor disponible.");
+        }
+    }
+
+    // Metodo Asignar Repartidor con nombre (sobrecarga)
+    @Override
+    public void asignarRepartidor(String nombreRepartidor) {
+        // Condicion
+        if (peso >= 100) {
+            System.out.println("Pedido " + getIdPedido() + " - Repartidor " + nombreRepartidor +
+                    " asignado. Encomienda de " + getPeso() + " kg, requiere transporte especial.");
+        } else {
+            System.out.println("Pedido " + getIdPedido() + ": Repartidor " + nombreRepartidor +
+                    " asignado. Encomienda peso normal.");
+        }
+    }
+
+}
